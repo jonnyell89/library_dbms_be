fd810e34c67bab9733417d37dbb4447220762059
jonnyell89 <jonnyell89@gmail.com>
2025-06-03 21:15:04 +0100

    initial commit - Spring Boot setup

---
+++ src/main/java/com/example/library_dbms_be/LibraryDbmsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_dbms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDbmsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryDbmsBeApplication.class, args);
+	}
+
+}
