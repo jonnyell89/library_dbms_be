caa9f9bb822168883dda163788c283170badbeca
jonnyell89 <jonnyell89@gmail.com>
2025-06-03 21:15:04 +0100

    initial commit - Spring Boot setup

---
+++ src/test/java/com/example/library_dbms_be/LibraryDbmsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_dbms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDbmsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
