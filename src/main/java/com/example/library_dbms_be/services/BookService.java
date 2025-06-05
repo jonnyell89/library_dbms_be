0db7e0ca81c35d7910cbdc11741fcaf483c366af
jonnyell89 <jonnyell89@gmail.com>
2025-06-05 22:17:31 +0100

    MemberService, BookService, ReservationService, ReservedBookService and AddressService added, derived query method added to AddressRepository

--- src/main/java/com/example/library_dbms_be/services/BookService.java
+++ src/main/java/com/example/library_dbms_be/services/BookService.java
@@ -1,4 +1,39 @@
 package com.example.library_dbms_be.services;
 
+import com.example.library_dbms_be.models.Book;
+import com.example.library_dbms_be.repositories.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+
+    public final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // CREATE
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    // READ
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long bookId) {
+        return bookRepository
+                .findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with bookId: " + bookId));
+    }
+
+    // DELETE
+    public void deleteBook(Long bookId) {
+        bookRepository.deleteById(bookId);
+    }
 }
--- src/main/java/com/example/library_dbms_be/services/MemberService.java
+++ src/main/java/com/example/library_dbms_be/services/MemberService.java
@@ -1,4 +1,59 @@
 package com.example.library_dbms_be.services;
 
+import com.example.library_dbms_be.models.Address;
+import com.example.library_dbms_be.models.Member;
+import com.example.library_dbms_be.repositories.AddressRepository;
+import com.example.library_dbms_be.repositories.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MemberService {
+
+    public final MemberRepository memberRepository;
+    public final AddressRepository addressRepository;
+
+    public MemberService(MemberRepository memberRepository, AddressRepository addressRepository) {
+        this.memberRepository = memberRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    // CREATE
+    public Member createMember(Member member) {
+
+        Address incomingAddress = member.getAddress();
+
+        // may or may not contain a null value
+        Optional<Address> existingAddress = addressRepository.findByLine1AndPostcode(
+                incomingAddress.getLine1(),
+                incomingAddress.getPostcode()
+        );
+
+        if (existingAddress.isPresent()) {
+            member.setAddress(existingAddress.get());
+        } else {
+            addressRepository.save(incomingAddress);
+        }
+
+        return memberRepository.save(member);
+    }
+
+    // READ
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member getMemberById(Long memberId) {
+        return memberRepository
+                .findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException("Member not found with memberId: " + memberId));
+    }
+
+    // DELETE
+    public void deleteMemberById(Long memberId) {
+        memberRepository.deleteById(memberId);
+    }
 }
