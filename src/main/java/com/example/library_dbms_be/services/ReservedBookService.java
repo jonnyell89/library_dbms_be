14972125365c286eb8028f0dbc1be9a63dafb889
jonnyell89 <jonnyell89@gmail.com>
2025-06-06 14:18:24 +0100

    MemberService, BookService, AddressService and ReservedBookService updated

--- src/main/java/com/example/library_dbms_be/services/ReservedBookService.java
+++ src/main/java/com/example/library_dbms_be/services/ReservedBookService.java
@@ -1,4 +1,22 @@
 package com.example.library_dbms_be.services;
 
+import com.example.library_dbms_be.repositories.BookRepository;
+import com.example.library_dbms_be.repositories.ReservationRepository;
+import com.example.library_dbms_be.repositories.ReservedBookRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservedBookService {
+
+    public final ReservedBookRepository reservedBookRepository;
+    public final ReservationRepository reservationRepository;
+    public final BookRepository bookRepository;
+
+    ReservedBookService(ReservedBookRepository reservedBookRepository, ReservationRepository reservationRepository, BookRepository bookRepository) {
+        this.reservedBookRepository = reservedBookRepository;
+        this.reservationRepository = reservationRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    
 }
