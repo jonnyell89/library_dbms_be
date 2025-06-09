d3b9a7c9e6d8103fa33e2c64a1c5f97b331b107c
jonnyell89 <jonnyell89@gmail.com>
2025-06-09 16:07:07 +0100

    basic AddressController logic added

--- src/main/java/com/example/library_dbms_be/controllers/AddressController.java
+++ src/main/java/com/example/library_dbms_be/controllers/AddressController.java
@@ -1,4 +1,46 @@
 package com.example.library_dbms_be.controllers;
 
+import com.example.library_dbms_be.models.Address;
+import com.example.library_dbms_be.services.AddressService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/addresses")
 public class AddressController {
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    // CREATE
+    @PostMapping // ("/api/addresses")
+    public Address createAddress(@RequestBody Address address) {
+        return addressService.createAddress(address);
+    }
+
+    // READ
+    @GetMapping // ("/api/addresses")
+    public List<Address> getAllAddresses() {
+        return addressService.getAllAddresses();
+    }
+
+    @GetMapping("/api/addresses/{id}")
+    public Address getAddressById(Long addressId) {
+        return addressService.getAddressById(addressId);
+    }
+
+    // UPDATE
+
+
+
+    // DELETE
+    @DeleteMapping("/api/addresses/{id}")
+    public String deleteAddressById(Long addressId) {
+        addressService.deleteAddressById(addressId);
+        return String.format("Address deleted with ID: " + addressId);
+    }
 }
