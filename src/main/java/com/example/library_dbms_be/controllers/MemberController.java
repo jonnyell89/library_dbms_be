0db92ddf02157d8fafaa7d653a51435ac29fbd6d
jonnyell89 <jonnyell89@gmail.com>
2025-06-09 15:55:03 +0100

    basic MemberController logic added

--- src/main/java/com/example/library_dbms_be/controllers/MemberController.java
+++ src/main/java/com/example/library_dbms_be/controllers/MemberController.java
@@ -1,4 +1,44 @@
 package com.example.library_dbms_be.controllers;
 
+import com.example.library_dbms_be.models.Member;
+import com.example.library_dbms_be.services.MemberService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // CREATE
+    @PostMapping // ("/api/members")
+    public Member createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+
+    // READ
+    @GetMapping // ("/api/members")
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    @GetMapping("/{id}") // ("/api/members/{id}")
+    public Member getMemberById(Long memberId) {
+        return memberService.getMemberById(memberId);
+    }
+
+    // UPDATE
+
+
+    // DELETE
+    @DeleteMapping("/{id}") // ("/api/members/{id}"
+    public String deleteCustomerById(@PathVariable Long memberId) {
+        return String.format("Member deleted with ID: " + memberId);
+    }
 }
