db67fac5d33c1456148ac268333112850f0fbe18
jonnyell89 <jonnyell89@gmail.com>
2025-06-09 16:21:51 +0100

    basic BookController logic added

--- src/main/java/com/example/library_dbms_be/controllers/BookController.java
+++ src/main/java/com/example/library_dbms_be/controllers/BookController.java
@@ -1,4 +1,46 @@
 package com.example.library_dbms_be.controllers;
 
+import com.example.library_dbms_be.models.Book;
+import com.example.library_dbms_be.services.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // CREATE
+    @PostMapping // ("/api/books")
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    // READ
+    @GetMapping // ("/api/books")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/api/books/{id}")
+    public Book getBookById(Long bookId) {
+        return bookService.getBookById(bookId);
+    }
+
+    // UPDATE
+
+
+
+    // DELETE
+    @DeleteMapping("/api/books/{id}")
+    public String deleteBookById(Long bookId) {
+        bookService.deleteBook(bookId);
+        return String.format("Book deleted with ID: " + bookId);
+    }
 }
